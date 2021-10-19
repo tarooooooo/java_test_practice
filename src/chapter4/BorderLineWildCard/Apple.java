928490c612d139e4caee15b3db3e29e542252ead
tarooooooo <ryutaromaeda83@gmail.com>
2021-10-19 20:56:08 +0900

    境界ワイルドカードの学習

---
+++ src/chapter4/BorderLineWildCard/Apple.java
@@ -0,0 +1,19 @@
+package chapter4.BorderLineWildCard;
+
+import java.math.BigDecimal;
+
+class Apple extends Fruit{
+	Apple(String name, BigDecimal price){
+		super(name, price);
+	}
+	
+	@Override
+	protected String getName() {
+		return name;
+	}
+	
+	@Override
+	protected BigDecimal getPrice(){
+		return price;
+	}
+}
---
+++ src/chapter4/BorderLineWildCard/wildcard.java
@@ -0,0 +1,9 @@
+package chapter4.BorderLineWildCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class wildcard {
+	List<? extends Fruit> fruits;
+	List<Apple> apples = new ArrayList<>();
+}
