c749da36d35f40c5c2eb0dd78a146292b96eff9e
tarooooooo <ryutaromaeda83@gmail.com>
2021-10-25 17:46:03 +0900

    [Add]JUnitを使用したテスト

---
+++ src/test/Calculator.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Calculator {
+	public int square(int x) {
+		return x * x;
+	}
+	
+	public double root (double x) {
+		if(x < 0) {
+			throw new IllegalArgumentException("負の値の平方根は求められません。");
+		}
+		return Math.sqrt(x);
+	}
+}
